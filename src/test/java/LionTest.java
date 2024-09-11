4b1d9ed090154d2cf36f66c846fc40423d49d289
Anton Starkov <st4rkovant@yandex.ru>
2024-09-11 22:00:37 +0500

    Sprint_6_ThirdRepair

--- src/test/java/LionTest.java
+++ src/test/java/LionTest.java
@@ -2,13 +2,13 @@ import com.example.Feline;
 import com.example.Lion;
 import org.junit.*;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LionTest {
-    @Spy
+    @Mock
     Feline feline;
     @Test
     public void lionsGetFoodMethodTest() throws Exception {
@@ -24,12 +24,12 @@ public class LionTest {
     }
     @Test
     public void lionsThrowExceptionTest() {
-        Assert.assertThrows(Exception.class, () -> {new Lion("");});
+        Assert.assertThrows(Exception.class, () -> {new Lion("", feline);});
     }
     @Test
     public void lionsThrowExceptionTextTest() {
         Assert.assertEquals("Используйте допустимые значения пола животного - самей или самка", (Assert.assertThrows(Exception.class, () -> {
-            new Lion("");}).getMessage()));
+            new Lion("", feline);}).getMessage()));
     }
 }
 
--- src/test/java/LionWithParameterizedTest.java
+++ src/test/java/LionWithParameterizedTest.java
@@ -1,13 +1,23 @@
+import com.example.Feline;
 import com.example.Lion;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 @RunWith(Parameterized.class)
 public class LionWithParameterizedTest {
     boolean expectedMane;
     String expectedSex;
+    @Mock
+    Feline feline;
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
     public LionWithParameterizedTest(boolean expectedMane, String expectedSex){
         this.expectedMane = expectedMane;
         this.expectedSex = expectedSex;
@@ -21,6 +31,6 @@ public class LionWithParameterizedTest {
     }
     @Test
     public void lionsDoesHaveManeMethodTest() throws Exception {
-        Assert.assertEquals(expectedMane, new Lion(expectedSex).doesHaveMane());
+        Assert.assertEquals(expectedMane, new Lion(expectedSex, feline).doesHaveMane());
     }
 }
